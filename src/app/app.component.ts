5d78a0c138a80e0b5869ed5631519ffb91ef2a77
Soman Anshuman <soman.a@idsil.loc>
2025-04-09 12:35:52 +0530

    day 3 basics of Signals

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, effect, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
+  count = signal(10);
+  x = 20;
 
+  constructor() {
+    effect(() => {
+      //this 'effect' runs if a signal is included below and is updated anytime
+      console.log('normal property = ', this.x);
+      console.log('signal = ', this.count());
+    });
+  }
+  updateValue() {
+    this.x = this.x + 1;
+    this.count.set(this.count() + 1);
+  }
 }
